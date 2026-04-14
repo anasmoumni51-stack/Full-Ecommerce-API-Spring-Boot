c78bd016d51c921222f3d6cd19761a22f4dd9db2
Anas Moumni <anas.moumni51@gmail.com>
2026-04-14 13:20:00 +0200

    DTOs and MapStruct object mapping

--- src/main/java/com/ecommercestore/store/controllers/UserController.java
+++ src/main/java/com/ecommercestore/store/controllers/UserController.java
@@ -1,37 +1,35 @@
 package com.ecommercestore.store.controllers;
 
-import com.ecommercestore.store.dtos.UserDto;
-import com.ecommercestore.store.mappers.UserMapper;
+import com.ecommercestore.store.entities.User;
 import com.ecommercestore.store.repositories.UserRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @AllArgsConstructor
 @RequestMapping("/users")
 public class UserController {
     private final UserRepository userRepository;
-    private final UserMapper userMapper;
 
     @GetMapping
-    public Iterable<UserDto> getAllUsers() {
-        return userRepository.findAll()
-                .stream()
-                .map(userMapper::toDto)
-                .toList();
+    public Iterable<User> getAllUsers() {
+        return userRepository.findAll();
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<UserDto> getUser(@PathVariable Long id) {
+    public ResponseEntity<User> getUser(@PathVariable Long id) {
         var user = userRepository.findById(id).orElse(null);
         if (user == null) {
             return ResponseEntity.notFound().build();
         }
 
-        return ResponseEntity.ok(userMapper.toDto(user));
+        return ResponseEntity.ok(user);
     }
 }
--- src/main/java/com/ecommercestore/store/repositories/UserRepository.java
+++ src/main/java/com/ecommercestore/store/repositories/UserRepository.java
@@ -1,8 +1,7 @@
 package com.ecommercestore.store.repositories;
 
 import com.ecommercestore.store.entities.User;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends CrudRepository<User, Long> {
 }
